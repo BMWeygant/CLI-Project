446cc8afe3d013a0111eed098651c34b859cd30a
Brandon Weygant <bobbydigital108@gmail.com>
2019-04-04 15:17:48 -0400

    Added a functioning CLI interface as well as defined several helper methods to clean up the 'run' method

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,16 +9,35 @@ attr_accessor :card_array
   end
 
   def run
+
+    welcome
+
+    card_list
+
+    input = gets.strip
+
+    card = @card_array[input.to_i - 1]
+    if !card
+      puts "Wtf are you talking about? Try again".
+      run
+    else
+      puts "#{card}"
+    end
+
+  end
+
+
+  def card_list
+    @card_array.each_with_index do |card, index|
+    puts "#{index+1}. #{card[:name]} - #{card[:program]}"
+    end
+  end
+
+  def welcome
     puts "Welcome!"
     puts "Here is a VERY breif list of the best cards avaialble for Madden Ultimate Team."
     puts "Enter the number next to the corresponding player name to view the cards information."
     puts "To gtfoh, just type 'gtfoh' at anytime."
-
-binding.pry
-    @card_array
-
-    #input = gets.strip
-
-
   end
+
 end
